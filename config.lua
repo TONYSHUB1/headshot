2e3629f70b44546ad18791acec4cc7a2ae749f38
TONYSHUB1 <133699186+TONYSHUB1@users.noreply.github.com>
2023-05-16 00:15:09 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.WeaponHashes = {
+    "WEAPON_PISTOL",
+    "WEAPON_COMBATPISTOL",
+    "WEAPON_APPISTOL"
+} -- Replace with the weapon hashes you want to use
+
+Config.HeadshotMultiplier = 2.0 -- Replace with the headshot multiplier you want to use
