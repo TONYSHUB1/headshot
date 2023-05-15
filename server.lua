7a1d42cfebf61b91e2acb51ffb6cf4535e0002a0
TONYSHUB1 <133699186+TONYSHUB1@users.noreply.github.com>
2023-05-16 00:54:08 +0530

    Delete server.lua

--- server.lua
+++
@@ -1,8 +0,0 @@
-RegisterServerEvent('headshotcontrol:getWeaponHashes')
-AddEventHandler('headshotcontrol:getWeaponHashes', function()
-    local weaponHashes = {}
-    for _, hash in ipairs(Config.WeaponHashes) do -- Loop through the weapon hashes in the config
-        table.insert(weaponHashes, GetHashKey(hash)) -- Add the hash to the table
-    end
-    TriggerClientEvent('headshotcontrol:setWeaponHashes', -1, weaponHashes)
-end)
